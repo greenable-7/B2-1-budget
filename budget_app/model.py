3d7ec0b6b4cb13b5b780e6cad46964c10b7d983c
greenable-7 <dreamitator@naver.com>
2026-09-19 01:35:52 +0900

    Define transaction model

--- budget_app/model.py
+++ budget_app/model.py
@@ -1,3 +1,13 @@
-# TODO: id, type, date, amount, category, memo, tags를 갖는 dataclass로 구현한다.
+# 거래 한 건의 id, type, date, amount, category, memo, tags를 관리한다.
 class Transaction:
-    pass
+    def __init__(self, transaction_id: str,transaction_type: str, date: str, amount: int, category: str, memo: str = "", tags: list[str] | None = None) -> None:
+        self.transaction_id = transaction_id
+        self.transaction_type = transaction_type
+        self.date = date
+        self.amount = amount
+        self.category = category
+        self.memo = memo
+        self.tag = tags or []
+
+
+ 
